11b9ca526720cc65cfe53dd062da90cc4a519e78
Caolan <barryc10@tcd.ie>
2019-10-15 00:11:14 +0100

    got location polled at a 0.5 second interval

--- screens/MainScreen/MainContainer.js
+++ screens/MainScreen/MainContainer.js
@@ -12,29 +12,41 @@ const initialState = {
   isRecording: false,
   status: 'Ready',
   username: '',
-  location: {},
+  intervalFunc: '',
+  location: { lat: '', long: '' },
 };
 
 const stateHandlers = {
   startRecording: () => () => ({ isRecording: true, status: 'Recording' }),
   stopRecording: () => () => ({ isRecording: false, status: 'Finished ' }),
   setLocation: () => ({ lat, long }) => ({ location: { lat, long } }),
+  setIntervalFunction: () => ({ funcId }) => ({ intervalFunc: funcId }),
+  clearIntervalFunction: () => () => ({ intervalFunc: null }),
+};
+
+const getLocation = async ({ setLocation }) => async () => {
+  const location = await Location.getCurrentPositionAsync({
+    accuracy: Location.Accuracy.Highest,
+  });
+  setLocation({ lat: location.coords.latitude.toString(), long: location.coords.longitude.toString() });
 };
 
 const handlers = {
-  record: ({ setLocation }) => async () => {
+  record: ({ setLocation, startRecording, setIntervalFunction }) => async () => {
     // https://docs.expo.io/versions/v35.0.0/sdk/location/
+    startRecording();
     const { status } = await Permissions.askAsync(Permissions.LOCATION);
     if (status !== 'granted') {
       console.log('no permission');
     }
-    const location = await Location.getCurrentPositionAsync({
-      accuracy: Location.Accuracy.Highest,
-    });
-    console.log(location);
-    setLocation({ lat: location.coords.latitude, long: location.coords.longitude });
+    const intervalFunction = setInterval(await getLocation({ setLocation }), 500);
+    setIntervalFunction({ funcId: intervalFunction });
   },
-  finish: () => () => {},
+  finish: (props) => () => {
+    const { stopRecording, intervalFunc } = props;
+    clearInterval(intervalFunc);
+    stopRecording();
+  },
 };
 
 export default compose(
