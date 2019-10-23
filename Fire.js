f8e66305f80f3492e7e25cd2558aab06520d8f4e
Caolan <barryc10@tcd.ie>
2019-10-23 21:06:33 +0100

    adding firebase

--- Fire.js
+++ Fire.js
@@ -1,4 +1,26 @@
-import firebase from 'firebase';
+import * as firebase from 'firebase';
+
+
+const app = firebase.initializeApp({
+  apiKey: 'AIzaSyA8NigRryFu6RCagi6JS1-lnYLyqyQwCVI',
+  authDomain: 'urban-application-7f91a.firebaseapp.com',
+  databaseURL: 'https://urban-application-7f91a.firebaseio.com',
+  projectId: 'urban-application-7f91a',
+  storageBucket: 'urban-application-7f91a.appspot.com',
+  messagingSenderId: '715635169669',
+  appId: '1:715635169669:web:68b4355735f376d4f1ccec',
+});
+
+const observeAuth = () => firebase.auth().onAuthStateChanged((user) => {
+  if(!user){
+    try {
+      firebase.auth().signInAnonymously();
+    }
+    catch ({ message }) {
+      alert(message);
+    }
+  }
+})
 
 class Fire {
   constructor() {
@@ -28,4 +50,21 @@ class Fire {
       }
     }
   };
+
+  get uid() {
+    return (firebase.auth().currentUser || {}).uid;
+  }
+
+  get ref() {
+    return firebase.database().ref('message');
+  }
+
+  on = callback => this.ref
+
+  send = locationData => {
+
+  }
 }
+
+Fire.shared = new Fire();
+export default Fire;
