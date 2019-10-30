43d4e7142f8a4ffbaf5fcf58bfc7b3f5d09cb2e0
Caolan <barryc10@tcd.ie>
2019-10-30 22:37:58 +0000

    have firebase databse working, now need to make interface

--- App.js
+++ App.js
@@ -2,13 +2,10 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
-import { Accuracy, watchPositionAsync } from 'expo-location';
 import { Sagas as rootSaga, Reducers } from './redux';
 import Navigator from './Navigator';
 
 const sagaMiddleware = createSagaMiddleware();
-
-
 
 const store = createStore(
   Reducers,
--- redux/sagas.js
+++ redux/sagas.js
@@ -1,10 +1,9 @@
 import {
-  put, takeEvery, all, call,
+  put, all, call,
 } from 'redux-saga/effects';
-import { readDirectoryAsync, documentDirectory } from 'expo-file-system';
 import { Accuracy, watchPositionAsync } from 'expo-location';
 import { requests, constants, Database } from '../Firebase';
-import { ACTION_TYPES, actionCreators } from './actions';
+import { actionCreators } from './actions';
 
 ///////////////
 // Workers ///
@@ -21,11 +20,6 @@ function* startWatchingLocation() {
   yield call(watchPositionAsync, { accuracy: Accuracy.Highest, timeInterval: 500 }, newLocation);
 }
 
-function* updateCSVFiles() {
-  const files = yield call(readDirectoryAsync, documentDirectory);
-  yield put(actionCreators.updateCSVFiles({ csvFiles: files }));
-}
-
 function* setStationData() {
   const stationData = yield call(requests.getAllStations);
   yield call(Database.setToDB, { data: stationData, table: constants.STATION_LIST });
@@ -40,22 +34,10 @@ function* readStationData() {
     yield put(actionCreators.updateStationList({ stations: stationDataVar }));
   }
 }
-///////////////
-// Watchers //
-//////////////
-function* watchLocationUpdate() {
-  yield takeEvery(ACTION_TYPES.NEW_LOCATION, newLocation);
-}
-
-function* watchCSVFilesUpdate() {
-  yield takeEvery(ACTION_TYPES.TRIGGER_UPDATE_CSV_FILES, updateCSVFiles);
-}
 
 export default function* rootSaga() {
   yield all([
     startWatchingLocation(),
-    watchCSVFilesUpdate(),
-    watchLocationUpdate(),
     setStationData(),
     readStationData(),
   ]);
