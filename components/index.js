b84c0f09411c8ad30c3954ecf042601811a17cac
Caolan <barryc10@tcd.ie>
2019-11-26 18:50:30 +0000

    more cleanup

--- components/index.js
+++ components/index.js
@@ -1,8 +1,8 @@
 import TrainItem from './TrainItem';
 import StationMap from './StationMap';
-import LateTrains from './LateTrains';
+
 import { NearbyStations, AllStations } from './StationDropdown';
 
 export {
-  TrainItem, StationMap, AllStations, NearbyStations, LateTrains,
+  TrainItem, StationMap, AllStations, NearbyStations,
 };
